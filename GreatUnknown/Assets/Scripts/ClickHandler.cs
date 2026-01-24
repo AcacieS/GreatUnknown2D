72a4245d9cdee4693b1f0da4bd12b2640c81b87a
AcacieS <acaciesong@gmail.com>
2026-01-24 15:10:43 -0500

    click handler

---
+++ GreatUnknown/Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClickHandler : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent _clicked;
+    private MouseInputProvider _mouse;
+    private BoxCollider2D _collider;
+    private void Awake()
+    {
+        _collider = GetComponent<BoxCollider2D>();
+        _mouse = FindObjectOfType<MouseInputProvider>();
+        _mouse.Clicked += MouseOnClicked;
+    }
+    private void MouseOnClicked()
+    {
+        if (_collider.bounds.Contains(_mouse.WorldPosition))
+        {
+            _clicked?.Invoke();
+        }
+    }
+}

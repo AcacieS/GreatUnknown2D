94f0940931b1cf460f71de4b3b9dd34b96f7c7a6
AcacieS <acaciesong@gmail.com>
2026-02-26 22:50:46 -0500

    something
    k

--- GreatUnknown/Assets/Scripts/GameManagement.cs
+++ GreatUnknown/Assets/Scripts/GameManagement.cs
@@ -42,12 +42,8 @@ public class GameManagement : MonoBehaviour
         {
             Debug.Log("Start Fish Game");
             // TODO: Start Sliding Game
-<<<<<<< Updated upstream
             SceneManager.LoadScene("SampleScene");
 
-=======
-            SceneManager.LoadSceneAsync("IceSlidingGame");
->>>>>>> Stashed changes
         }
     }
     public void NextDay()

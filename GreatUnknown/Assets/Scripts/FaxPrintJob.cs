bcf95308c99ed98b3362e9fa535c93ec0b9f3217
Locrium <112211021+Locrium@users.noreply.github.com>
2026-02-27 19:04:32 -0500

    Reapply "Fix FaxState compilation issues, improve encapsulation, and correct logging usage"
    This reverts commit 35b3916fece1cb89d84bf51e8f1ed8c3918464d1.

--- GreatUnknown/Assets/Scripts/FaxPrintJob.cs
+++ GreatUnknown/Assets/Scripts/FaxPrintJob.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
-using System;
 
 [CreateAssetMenu(menuName = "Fax/Fax Print Job")]
 public class FaxPrintJob : ScriptableObject
 {
-    [SerializeField] private Sprite2D sprite;
-    [SerializeField] private String header;
-    [SerializeField] private String body;
+    [SerializeField] private Sprite sprite;
+    [SerializeField] private string header;
+    [SerializeField] private string body;
+
+    public Sprite Sprite => sprite;
+    public string Header => header;
+    public string Body => body;
 }

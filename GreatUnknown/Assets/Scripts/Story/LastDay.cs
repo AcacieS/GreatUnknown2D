cdc28eb6d950a8effc4f42321f5d894171f9b3ab
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-03-16 00:53:09 -0400

    Add Ambience to LastDay And Fix Animator

--- GreatUnknown/Assets/Scripts/Story/LastDay.cs
+++ GreatUnknown/Assets/Scripts/Story/LastDay.cs
@@ -7,17 +7,22 @@ public class LastDay : MonoBehaviour
     public static LastDay Instance {get; private set;}
     [SerializeField] private int klaxonDelaySeconds = 30;
     [SerializeField] private AudioInfo Klaxon;
+    [SerializeField] private AudioInfo LastDayAmbiance;
     [SerializeField] private GameObject LastDayTerminal;
+
+    private bool lastDayHit = false;
 
     void Awake()
     {
         Instance = this;
     }
 
-    void OnEnable()
+    void Update()
     {
-        if (GameManagement.Instance.GetNbDayLeft() == 0)
+        if (GameManagement.Instance.GetNbDayLeft() == 0 && !lastDayHit)
         {
+            lastDayHit = true;
+            SoundManager.instance.PlaySound(LastDayAmbiance);
             StartCoroutine(DelayKlaxonSound());
         }
     }

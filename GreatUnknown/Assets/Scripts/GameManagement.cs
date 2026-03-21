ac89fe5829c5a7c8d2e66953776ea0448adbbae2
Jonathan Sandjong <232967936+J-on-S@users.noreply.github.com>
2026-03-21 16:55:18 -0400

    Fix whitespace in GameManagement.cs

--- GreatUnknown/Assets/Scripts/GameManagement.cs
+++ GreatUnknown/Assets/Scripts/GameManagement.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using NUnit.Framework;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class GameManagement : MonoBehaviour
 {
@@ -238,8 +235,7 @@ public class GameManagement : MonoBehaviour
 
         dayAnimation.WriteText();
     }
-  
-    
+
     public void SpecialEventDay()
     {
         switch (nbDaysPassed)

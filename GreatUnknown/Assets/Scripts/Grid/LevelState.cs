de22822c8c4bbe30d091880d9c2f4a7f3ad4b5c5
Locrium <112211021+Locrium@users.noreply.github.com>
2026-03-22 20:54:12 -0400

    I LOVE BANGERS SDF 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥

--- GreatUnknown/Assets/Scripts/Grid/LevelState.cs
+++ GreatUnknown/Assets/Scripts/Grid/LevelState.cs
@@ -4,12 +4,18 @@ using System;
 public class LevelState : MonoBehaviour
 {
     public bool GameOver { get; private set; }
-
     public event Action OnGameOver;
 
     public void SetGameOver()
     {
         if (GameOver) return;   // prevent double trigger
+
+        if (GameManagement.Instance !=null)
+        {
+            GameManagement.Instance.MarkSlidingGameFinished();
+            Debug.Log("GameFisnishedOnGameMangement");
+
+        }
 
         GameOver = true;
         Debug.Log("GOAL REACHED — GAME OVER");
